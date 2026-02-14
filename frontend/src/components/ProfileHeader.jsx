363b812b8a2dd8752db1e749bb8ec5763f6202a0
Hassan-Ali362 <malikhass1357@gmail.com>
2026-02-14 23:47:53 +0500

    some changes in code

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -3,24 +3,24 @@ import { useChatStore } from "../store/useChatStore";
 import { useAuthStore } from "../store/useAuthStore";
 import { LogOutIcon, Volume2Icon, VolumeOffIcon } from "lucide-react";
 
-const mouseClickSound = new Audio("/sounds/mouseClickSound.mp3");
+const mouseClickSound = new Audio("/sounds/mouseClickSound.mp3");  // Creates an audio instance, Preload the sound and keep it ready to play. This is browser JavaScript, not React-specific.
 
 export const ProfileHeader = () => {
-  const { logout, authUser, updateProfile } = useAuthStore();
-  const { isSoundEnabled, toggleSound } = useChatStore();
+  const { logout, authUser, updateProfile } = useAuthStore();  // authUser is the current authenticated user, logout is a function to log out the user, and updateProfile is a function to update the user's profile information. These are all provided by the useAuthStore hook, which manages authentication state and actions in the application.
+  const { isSoundEnabled, toggleSound } = useChatStore();  
   const [selectedImg, setSelectedImg] = useState(null);
 
-  const fileInputRef = useRef(null);
+  const fileInputRef = useRef(null);  // This creates a reference to the file input element --> <input type="file" />., allowing us to programmatically trigger a click on it when the user clicks the avatar button.
 
-  const handleImageClick = () => {
+  const handleImageClick = () => {    // fileInputRef.current will point to the actual DOM element. When we call fileInputRef.current.click(), it simulates a click on the hidden file input, which opens the file picker dialog for the user to select an image.
     fileInputRef.current.click();
   };
-
+ 
   const handleImageUpload = (e) => {
-    const file = e.target.files[0];
+    const file = e.target.files[0];    // Gets selected file from input. 
     if (file) {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
+      const reader = new FileReader();  // Creates a new FileReader instance to read the selected file. The FileReader API allows us to read the contents of files stored on the user's computer asynchronously, without blocking the main thread. In this case, we use it to read the selected image file as a data URL, which can be used to display the image in the UI and send it to the server for updating the user's profile picture.
+      reader.readAsDataURL(file);  // Reads the selected file as a data URL (base64 encoded string). This allows us to display the image immediately in the UI and also send it to the server for updating the user's profile picture.
 
       reader.onloadend = async () => {
         setSelectedImg(reader.result);
