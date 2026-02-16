1fc5d4d2fc6aec5214ca40bf5cc43594cf528e76
Hassan-Ali362 <malikhass1357@gmail.com>
2026-02-16 23:40:51 +0500

    usersLoading skeleton component made

--- frontend/src/components/ChatsList.jsx
+++ frontend/src/components/ChatsList.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import { UserLoadingSkeleton } from "./UserLoadingSkeleton";
+import { NoChatsFound } from "../components/NoChatsFounds"
 
 export const ChatsList = () => {
+
+  const {getMyChatPartners, chats, isUsersLoading, setSelectedUser} = useChatStore();
+
+  useEffect(()=>{
+    getMyChatPartners()
+  }, [getMyChatPartners])
+
+  if(isUsersLoading) return <UserLoadingSkeleton/>
+  if(chats.length == 0) return <NoChatsFound />
+
   return (
     <div className="flex items-center justify-center h-screen">
       <p className="text-slate-400">No conversation selected</p>
---
+++ frontend/src/components/NoChatsFound.jsx
@@ -0,0 +1,15 @@
+import { MessageCircleIcon } from "lucide-react"
+
+export const NochatsFound = () => {
+    return(
+        <>
+        <div className="flex flex-col items-center justify-center py-10 text-center space-y-4">
+
+            <div className="w-16 h-16">
+
+            </div>
+
+        </div>
+        </>
+    )
+}
