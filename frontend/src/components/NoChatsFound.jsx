596d9192b20c5f7b93378da6a049f4d1c563d0e0
Hassan-Ali362 <malikhass1357@gmail.com>
2026-02-19 00:32:51 +0500

    done all

--- frontend/src/components/NoChatsFound.jsx
+++ frontend/src/components/NoChatsFound.jsx
@@ -4,7 +4,7 @@ const NochatsFound = () => {
     return(
         <>
         <div className="flex flex-col items-center py-10 text-center space-y-4">
-
+                      
             <div className="w-16 h-16 bg-cyan-500/10 rounded-full flex items-center justify-center">
                 <MessageCircleIcon className="w-8 h-8 text-cyan-400"/>
             </div>
